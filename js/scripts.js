64ff54d604456bdddc6db8ef8227916f812b2777
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-24 22:53:30 +0700

    add backtotop

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,30 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
+
+
+    $(document).ready(function(){
+ 
+        //Check to see if the window is top if not then display button
+        $(window).scroll(function(){
+       
+         // Show button after 100px
+         var showAfter = 800;
+         if ( $(this).scrollTop() > showAfter ) { 
+          $('.back-to-top').fadeIn();
+         } else { 
+          $('.back-to-top').fadeOut();
+         }
+        });
+        
+        //Click event to scroll to top
+        $('.back-to-top').click(function(){
+         $('html, body').animate({scrollTop : 700},2000);
+         return false;
+        });
+        
+       });
+
+
+   
 })(jQuery); // End of use strict
